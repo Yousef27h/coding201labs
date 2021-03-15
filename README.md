d37c202005620d16696df1cd06949e7e5d2c2da6
Yousef27h <yousef_salem97@yahoo.com>
2021-03-15 14:56:18 +0200

    new web page

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # coding201labs
+
+
+My name is *Yousef Salem*, I have a bachelors degree in Mechanical engineering. Currently enrolled in an intensive course in Software Development which is provided by Abdul Aziz Ghurair School of Advanced Computing (_ASAC_) in collaboration with _Code Fellows Academy_ in Seattle – US. 
+This repository, contains my project work throughout labs during *code201* course.
+In this repository I mainly used HTML, CSS, and JavaScript to complete my labs
+
+**MIT ©** Yousef Salem
---
+++ js/app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+let ocean = prompt('The ocean is blue');
+if (ocean.toLowerCase() === 'yes' || ocean.toLowerCase() === 'y'){
+  alert('You are right!');
+  // console.log('You are right! The ocean is blue');
+}else{
+  alert('You are wrong!');
+}
+
+let smart = prompt('I\'m smarter than you');
+if (smart.toLowerCase() === 'yes' || smart.toLowerCase() === 'y'){
+  alert('You are right!');
+  // console.log('You are right! I\'m smarter than you');
+}else{
+  alert('You are wrong!');
+}
+
+let place = prompt('I live in Jordan');
+if (place.toLowerCase() === 'yes' || place.toLowerCase() === 'y'){
+  alert('You are right!');
+  // console.log('You are right! I live in Jordan');
+}else{
+  alert('You are wrong!');
+}
+
+let lang = prompt('I can speak English');
+if (lang.toLowerCase() === 'yes' || lang.toLowerCase() === 'y'){
+  alert('You are right!');
+  // console.log('You are right! I can speak English');
+}else{
+  alert('You are wrong!');
+}
+
+let prolang = prompt('I created this webpage using HTML');
+if (prolang.toLowerCase() === 'yes' || prolang.toLowerCase() === 'y'){
+  alert('You are right!');
+  // console.log('You are right! I created this webpage using HTML');
+}else{
+  alert('You are wrong!');
+}
+
+
+let name= prompt('What\'s your name?');
+alert('Your name is '+ name + '!');
+document.write('Hello '+ name + '! and welcome to my web page');
+
